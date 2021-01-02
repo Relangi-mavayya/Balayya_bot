ab356f545d3800de478d3d482f669b890cbcddf6
Relangi-mavayya <76868061+Relangi-mavayya@users.noreply.github.com>
2021-01-02 18:59:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def on_ready():
     print('Bot is ready')
 
 dailouge_pics =[
-'https://cdn.discordapp.com/attachments/794846731296178207/794872496503259166/B-10.png',
+'https://cdn.discordapp.com/attachments/794846731296178207/794872496503259166/B-10.png', 
 'https://cdn.discordapp.com/attachments/794846731296178207/794872477376315432/B-2.png',
 'https://cdn.discordapp.com/attachments/794846731296178207/794872462164623370/B-3.png',
 'https://cdn.discordapp.com/attachments/794846731296178207/794872462164623370/B-3.png',
@@ -19,7 +19,7 @@ dailouge_pics =[
 'https://cdn.discordapp.com/attachments/794846731296178207/794872385815183390/B-7.png',
 'https://cdn.discordapp.com/attachments/794846731296178207/794872366248624128/B-8.png',
 'https://cdn.discordapp.com/attachments/794846731296178207/794872352939835403/Balayyya-Babu.png'
-]
+]           #List of pictures
 
 
 @client.command()
