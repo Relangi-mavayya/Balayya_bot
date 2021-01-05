4cf4f528e13b6b6c0201aec9aca246b47e82a3b3
Relangi-mavayya <76868061+Relangi-mavayya@users.noreply.github.com>
2021-01-05 15:22:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,19 @@ movie_dialouge =[
 'https://github.com/Relangi-mavayya/Balayya_bot/blob/main/Assets/moviedialogs/9.-Central-Legend.jpg?raw=true'
 ]
 
+slogans=[
+'**Jublie hills Banjara hills Balayya kodithe hospital bills**',
+'**Hyderabad secunderabad balayya Babu zindabad**',
+'**Ramudu beemudu ma balayya Babu demudu**',
+'**Oopu groupu balayya Babu thopu**',
+'**Varshakalam lo current kotha Ammaila gundello balayya Babu motha**',
+'**India ki pm modi industry ki balayya Babu daddy**',
+'**1234 balayya never before**',
+'**Maza frooti balayya Babu naughty**',
+'**Jiljil jiga ma balayya Babu sega**',
+'**Uppukaaram balayya meda chaavadu ma mamakaram**',
+'**Paina kinda oopu balayya Babu thopu**'
+]
 
 import discord
 from discord.ext import commands
@@ -99,7 +112,7 @@ async def greet(ctx, arg):
     random_greet = random.choice(greeting_gifs)
     embed.set_image(url= random_greet)
     embed.set_footer(icon_url=ctx.author.avatar_url, text= f'Requested by {ctx.author.name}')
-    await ctx.send( str(arg)+ ' **Namaskaram :pray: **', embed = embed)
+    await ctx.send( '**Namaskaram :pray: ** ' +str(arg) , embed = embed)
 
 #if someone didnt mention someone
 @client.event
@@ -109,39 +122,48 @@ async def on_command_error(ctx, error):
     embed = discord.Embed(description = msg, color = value)
     if isinstance(error,commands.MissingRequiredArgument):
         await ctx.send(f'{ctx.author.mention}', embed=embed)
-# My sample help command:
-@bot.command()
-async def help(ctx, args=None):
-    help_embed = discord.Embed(title="My Bot's Help!")
+
+
+#for random fun dailouge pics
+@client.command(aliases=['s',])
+async def slogan(ctx):
+    value = random.randint(0, 0xffffff)
+    random_slogan = random.choice(slogans)
+    embed = discord.Embed(description = random_slogan, color = value)
+    await ctx.send(embed = embed)
+
+
+
+"""# My sample help command:
+@client.command(aliases=['help','h'])
+async def _help(ctx, args=None):
+    help_embed = discord.Embed(title="Balayya Help Commands")
     command_names_list = [x.name for x in bot.commands]
 
-    # If there are no arguments, just list the commands:
-    if not args:
-        help_embed.add_field(
-            name="List of supported commands:",
-            value="\n".join([str(i+1)+". "+x.name for i,x in enumerate(bot.commands)]),
-            inline=False
-        )
-        help_embed.add_field(
-            name="Details",
-            value="Type `.help <command name>` for more details about each command.",
-            inline=False
-        )
+# If there are no arguments, just list the commands:
+if not args:
+    help_embed.add_field(name="List of supported commands:", value="\n".join([str(i+1)+". "+x.name for i,x in enumerate(bot.commands)]),inline=False)
+    help_embed.add_field(name="Details", value="Type `.help <command name>` for more details about each command.", inline=False)
 
-    # If the argument is a command, get the help text from that command:
-    elif args in command_names_list:
-        help_embed.add_field(
-            name=args,
-            value=bot.get_command(args).help
-        )
+# If the argument is a command, get the help text from that command:
+elif args in command_names_list:
+    help_embed.add_field(name=args, value=bot.get_command(args).help)
 
-    # If someone is just trolling:
-    else:
-        help_embed.add_field(
-            name="Nope.",
-            value="Don't think I got that command, boss!"
-        )
+# If someone is just trolling:
+else:
+    help_embed.add_field(name="Nope.", value="Don't think I got that command, boss!")
+    await ctx.send(embed=help_embed)"""
 
-    await ctx.send(embed=help_embed)
-    
+
+class MyHelpCommand(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        e = discord.Embed(color=discord.Color.blurple(), description='__**Balayya a telugu film god**__ \n') 
+        for page in self.paginator.pages:
+            e.description += page
+        await destination.send(embed=e)
+
+client.help_command = MyHelpCommand()
+
+
 client.run('token')
